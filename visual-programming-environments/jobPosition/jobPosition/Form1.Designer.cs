c2e26633254b5d0ca4ae564e9c3928d7e9cb5d7e
Eleonora Dimchevska <e.dimchevska@gmail.com>
2015-05-29 15:22:20 +0300

    one more task from one more discipline and git ignore file

---
+++ visual-programming-environments/jobPosition/jobPosition/Form1.Designer.cs
@@ -0,0 +1,373 @@
+﻿namespace jobPosition
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.formPanel = new System.Windows.Forms.Panel();
+            this.de_checkBox = new System.Windows.Forms.CheckBox();
+            this.fr_checkBox = new System.Windows.Forms.CheckBox();
+            this.eng_checkBox = new System.Windows.Forms.CheckBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.MS_Access_textBox = new System.Windows.Forms.TextBox();
+            this.MS_Excel_textBox = new System.Windows.Forms.TextBox();
+            this.MS_PowerPoint_textBox = new System.Windows.Forms.TextBox();
+            this.MS_Word_textBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.idTextBox = new System.Windows.Forms.TextBox();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.resultPanel = new System.Windows.Forms.Panel();
+            this.makeTextBoldButton = new System.Windows.Forms.Button();
+            this.resultButton = new System.Windows.Forms.Button();
+            this.resultPonts_textBox = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.resoneDataTextBox = new System.Windows.Forms.TextBox();
+            this.formPanel.SuspendLayout();
+            this.resultPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // formPanel
+            // 
+            this.formPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.formPanel.Controls.Add(this.de_checkBox);
+            this.formPanel.Controls.Add(this.fr_checkBox);
+            this.formPanel.Controls.Add(this.eng_checkBox);
+            this.formPanel.Controls.Add(this.label10);
+            this.formPanel.Controls.Add(this.label7);
+            this.formPanel.Controls.Add(this.label6);
+            this.formPanel.Controls.Add(this.label5);
+            this.formPanel.Controls.Add(this.label4);
+            this.formPanel.Controls.Add(this.MS_Access_textBox);
+            this.formPanel.Controls.Add(this.MS_Excel_textBox);
+            this.formPanel.Controls.Add(this.MS_PowerPoint_textBox);
+            this.formPanel.Controls.Add(this.MS_Word_textBox);
+            this.formPanel.Controls.Add(this.label3);
+            this.formPanel.Controls.Add(this.idTextBox);
+            this.formPanel.Controls.Add(this.nameTextBox);
+            this.formPanel.Controls.Add(this.clearButton);
+            this.formPanel.Controls.Add(this.label2);
+            this.formPanel.Controls.Add(this.label1);
+            this.formPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.formPanel.Location = new System.Drawing.Point(0, 0);
+            this.formPanel.Name = "formPanel";
+            this.formPanel.Size = new System.Drawing.Size(820, 211);
+            this.formPanel.TabIndex = 0;
+            // 
+            // de_checkBox
+            // 
+            this.de_checkBox.AutoSize = true;
+            this.de_checkBox.Location = new System.Drawing.Point(650, 150);
+            this.de_checkBox.Name = "de_checkBox";
+            this.de_checkBox.Size = new System.Drawing.Size(64, 17);
+            this.de_checkBox.TabIndex = 17;
+            this.de_checkBox.Text = "немски";
+            this.de_checkBox.UseVisualStyleBackColor = true;
+            // 
+            // fr_checkBox
+            // 
+            this.fr_checkBox.AutoSize = true;
+            this.fr_checkBox.Location = new System.Drawing.Point(650, 111);
+            this.fr_checkBox.Name = "fr_checkBox";
+            this.fr_checkBox.Size = new System.Drawing.Size(70, 17);
+            this.fr_checkBox.TabIndex = 16;
+            this.fr_checkBox.Text = "френски";
+            this.fr_checkBox.UseVisualStyleBackColor = true;
+            // 
+            // eng_checkBox
+            // 
+            this.eng_checkBox.AutoSize = true;
+            this.eng_checkBox.Location = new System.Drawing.Point(650, 75);
+            this.eng_checkBox.Name = "eng_checkBox";
+            this.eng_checkBox.Size = new System.Drawing.Size(79, 17);
+            this.eng_checkBox.TabIndex = 15;
+            this.eng_checkBox.Text = "английски";
+            this.eng_checkBox.UseVisualStyleBackColor = true;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(669, 45);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(61, 13);
+            this.label10.TabIndex = 14;
+            this.label10.Text = "Чужд език";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(294, 176);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(67, 13);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "MS_Access ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(294, 141);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(58, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "MS_Excel ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(44, 173);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(86, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "MS_PowerPoint ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(72, 141);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "MS_Word ";
+            // 
+            // MS_Access_textBox
+            // 
+            this.MS_Access_textBox.Location = new System.Drawing.Point(367, 173);
+            this.MS_Access_textBox.MaxLength = 2;
+            this.MS_Access_textBox.Name = "MS_Access_textBox";
+            this.MS_Access_textBox.Size = new System.Drawing.Size(100, 20);
+            this.MS_Access_textBox.TabIndex = 9;
+            this.MS_Access_textBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox_onlyNumbers);
+            this.MS_Access_textBox.Validating += new System.ComponentModel.CancelEventHandler(this.textBox_Validating);
+            // 
+            // MS_Excel_textBox
+            // 
+            this.MS_Excel_textBox.Location = new System.Drawing.Point(367, 134);
+            this.MS_Excel_textBox.MaxLength = 2;
+            this.MS_Excel_textBox.Name = "MS_Excel_textBox";
+            this.MS_Excel_textBox.Size = new System.Drawing.Size(100, 20);
+            this.MS_Excel_textBox.TabIndex = 8;
+            this.MS_Excel_textBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox_onlyNumbers);
+            this.MS_Excel_textBox.Validating += new System.ComponentModel.CancelEventHandler(this.textBox_Validating);
+            // 
+            // MS_PowerPoint_textBox
+            // 
+            this.MS_PowerPoint_textBox.Location = new System.Drawing.Point(165, 173);
+            this.MS_PowerPoint_textBox.MaxLength = 2;
+            this.MS_PowerPoint_textBox.Name = "MS_PowerPoint_textBox";
+            this.MS_PowerPoint_textBox.Size = new System.Drawing.Size(100, 20);
+            this.MS_PowerPoint_textBox.TabIndex = 7;
+            this.MS_PowerPoint_textBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox_onlyNumbers);
+            this.MS_PowerPoint_textBox.Validating += new System.ComponentModel.CancelEventHandler(this.textBox_Validating);
+            // 
+            // MS_Word_textBox
+            // 
+            this.MS_Word_textBox.Location = new System.Drawing.Point(165, 134);
+            this.MS_Word_textBox.MaxLength = 2;
+            this.MS_Word_textBox.Name = "MS_Word_textBox";
+            this.MS_Word_textBox.Size = new System.Drawing.Size(100, 20);
+            this.MS_Word_textBox.TabIndex = 6;
+            this.MS_Word_textBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox_onlyNumbers);
+            this.MS_Word_textBox.Validating += new System.ComponentModel.CancelEventHandler(this.textBox_Validating);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(45, 115);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "точки то тестове";
+            // 
+            // idTextBox
+            // 
+            this.idTextBox.Location = new System.Drawing.Point(165, 72);
+            this.idTextBox.MaxLength = 4;
+            this.idTextBox.Name = "idTextBox";
+            this.idTextBox.Size = new System.Drawing.Size(100, 20);
+            this.idTextBox.TabIndex = 4;
+            this.idTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox_onlyNumbers);
+            this.idTextBox.Validating += new System.ComponentModel.CancelEventHandler(this.textBox_Validating);
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(165, 45);
+            this.nameTextBox.MaxLength = 30;
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(302, 20);
+            this.nameTextBox.TabIndex = 3;
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(392, 13);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(75, 23);
+            this.clearButton.TabIndex = 2;
+            this.clearButton.Text = "New";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(45, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Входящ номер ID ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(45, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Три имена";
+            // 
+            // resultPanel
+            // 
+            this.resultPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.resultPanel.Controls.Add(this.makeTextBoldButton);
+            this.resultPanel.Controls.Add(this.resultButton);
+            this.resultPanel.Controls.Add(this.resultPonts_textBox);
+            this.resultPanel.Controls.Add(this.label9);
+            this.resultPanel.Controls.Add(this.label8);
+            this.resultPanel.Controls.Add(this.resoneDataTextBox);
+            this.resultPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.resultPanel.Location = new System.Drawing.Point(0, 211);
+            this.resultPanel.Name = "resultPanel";
+            this.resultPanel.Size = new System.Drawing.Size(820, 153);
+            this.resultPanel.TabIndex = 1;
+            // 
+            // makeTextBoldButton
+            // 
+            this.makeTextBoldButton.Location = new System.Drawing.Point(733, 114);
+            this.makeTextBoldButton.Name = "makeTextBoldButton";
+            this.makeTextBoldButton.Size = new System.Drawing.Size(75, 23);
+            this.makeTextBoldButton.TabIndex = 19;
+            this.makeTextBoldButton.Text = "Bold";
+            this.makeTextBoldButton.UseVisualStyleBackColor = true;
+            this.makeTextBoldButton.Click += new System.EventHandler(this.makeTextBoldButton_Click);
+            // 
+            // resultButton
+            // 
+            this.resultButton.Location = new System.Drawing.Point(606, 114);
+            this.resultButton.Name = "resultButton";
+            this.resultButton.Size = new System.Drawing.Size(75, 23);
+            this.resultButton.TabIndex = 18;
+            this.resultButton.Text = "Резултати";
+            this.resultButton.UseVisualStyleBackColor = true;
+            this.resultButton.Click += new System.EventHandler(this.resultButton_Click);
+            // 
+            // resultPonts_textBox
+            // 
+            this.resultPonts_textBox.Location = new System.Drawing.Point(165, 112);
+            this.resultPonts_textBox.Name = "resultPonts_textBox";
+            this.resultPonts_textBox.Size = new System.Drawing.Size(100, 20);
+            this.resultPonts_textBox.TabIndex = 17;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(51, 115);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(88, 13);
+            this.label9.TabIndex = 16;
+            this.label9.Text = "Общ брой точки";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(48, 25);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(111, 13);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "Данни за кандидата";
+            // 
+            // resoneDataTextBox
+            // 
+            this.resoneDataTextBox.Location = new System.Drawing.Point(165, 6);
+            this.resoneDataTextBox.Multiline = true;
+            this.resoneDataTextBox.Name = "resoneDataTextBox";
+            this.resoneDataTextBox.Size = new System.Drawing.Size(302, 78);
+            this.resoneDataTextBox.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(820, 364);
+            this.Controls.Add(this.resultPanel);
+            this.Controls.Add(this.formPanel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.formPanel.ResumeLayout(false);
+            this.formPanel.PerformLayout();
+            this.resultPanel.ResumeLayout(false);
+            this.resultPanel.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel formPanel;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox MS_Access_textBox;
+        private System.Windows.Forms.TextBox MS_Excel_textBox;
+        private System.Windows.Forms.TextBox MS_PowerPoint_textBox;
+        private System.Windows.Forms.TextBox MS_Word_textBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox idTextBox;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel resultPanel;
+        private System.Windows.Forms.Button resultButton;
+        private System.Windows.Forms.TextBox resultPonts_textBox;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox resoneDataTextBox;
+        private System.Windows.Forms.CheckBox de_checkBox;
+        private System.Windows.Forms.CheckBox fr_checkBox;
+        private System.Windows.Forms.CheckBox eng_checkBox;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Button makeTextBoldButton;
+    }
+}
+
---
+++ visual-programming-environments/jobPosition/jobPosition/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace jobPosition
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            resultButton.Enabled = false;
+            //idTextBox.Tag = false;
+            //MS_Word_textBox.Tag = false;
+            //MS_PowerPoint_textBox.Tag = false;
+            //MS_Excel_textBox.Tag = false;
+            //MS_Access_textBox.Tag = false;
+
+            RecursiveClearTextBoxes(this.Controls);
+        }
+
+        private void ValidateOK()
+        {
+            this.resultButton.Enabled = ((bool)(this.idTextBox.Tag)
+                                            && (bool)(this.idTextBox.Tag)
+                                            && (bool)(this.MS_Word_textBox.Tag)
+                                            && (bool)(this.MS_PowerPoint_textBox.Tag)
+                                            && (bool)(this.MS_Excel_textBox.Tag)
+                                            && (bool)(this.MS_Excel_textBox.Tag)
+                                            && (bool)(this.MS_Access_textBox.Tag));
+        }
+        private void resultButton_Click(object sender, EventArgs e)
+        {
+            this.resoneDataTextBox.Text = "Име: " + this.nameTextBox.Text + ";\r\nВходящ номер: " + this.idTextBox.Text;
+
+            int pointsResult = int.Parse(this.MS_Word_textBox.Text) + int.Parse(this.MS_PowerPoint_textBox.Text);
+                pointsResult += int.Parse(this.MS_Excel_textBox.Text) + int.Parse(this.MS_Access_textBox.Text);
+
+            // adding extra point for knowledge of languages
+            if (this.eng_checkBox.Checked == true)
+                pointsResult += 2;
+            if (this.fr_checkBox.Checked == true)
+                pointsResult += 2;
+            if (this.de_checkBox.Checked == true)
+                pointsResult += 2;
+
+            this.resultPonts_textBox.Text = String.Format("{0}", pointsResult);
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            RecursiveClearTextBoxes(this.Controls);
+            resultButton.Enabled = false;
+            setCheckBoxesUnChecked(this);
+        }
+
+        //reset all checkboxes
+        private void setCheckBoxesUnChecked(Control parent)
+        {
+            foreach (Control ctrl in parent.Controls)
+            {
+                if (ctrl is CheckBox)
+                    ((CheckBox)ctrl).Checked = false;
+
+                setCheckBoxesUnChecked(ctrl);
+            }
+        }
+
+        //reset all textboxes
+        private void RecursiveClearTextBoxes(Control.ControlCollection cc)
+        {
+            foreach (Control ctrl in cc)
+            {
+                TextBox tb = ctrl as TextBox;
+                if (tb != null)
+                {
+                    tb.Clear();
+                    tb.BackColor = System.Drawing.SystemColors.Window;
+                    tb.Tag = false;
+                }
+                else
+                    RecursiveClearTextBoxes(ctrl.Controls);
+            }
+        }
+
+        private void textBox_Validating(object sender, CancelEventArgs e)
+        {
+            TextBox tb = (TextBox)sender;
+            if (tb.Text.Length == 0)
+            {
+                tb.BackColor = Color.Red;
+                tb.Tag = false;
+            }
+            else
+            {
+                tb.BackColor = System.Drawing.SystemColors.Window;
+                tb.Tag = true;
+            }
+            ValidateOK();
+        }
+
+        // only numbers
+        private void textBox_onlyNumbers(object sender, KeyPressEventArgs e)
+        {
+            char ch = e.KeyChar;
+
+            if(!Char.IsDigit(ch) && ch != 8)
+            {
+                e.Handled = true;
+            }
+        }
+
+        //make text bold
+        private void makeTextBoldButton_Click(object sender, EventArgs e)
+        {
+            resoneDataTextBox.Font = new Font(resoneDataTextBox.Font, FontStyle.Bold);
+        }
+
+    }
+}
